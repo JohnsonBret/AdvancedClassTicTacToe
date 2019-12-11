b21319d7d4839d43f98d06efac06a05a5b6c8c7d
Bret <bretaudio@gmail.com>
2019-12-10 18:11:28 -0800

    turn off database

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TicTacToe", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true});
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+// mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb://localhost:27017/TicTacToe", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true});
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
 
 module.exports = {
     mongoose: mongoose
