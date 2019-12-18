1b50fccb63915e9fc81a8dbd94683bf0baea2fa4
Bret <bretaudio@gmail.com>
2019-12-17 18:23:08 -0800

    updated connection string

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; 
 mongoose.connect("mongodb+srv://ucode_hermosa:SxaI2xLwgwY8KmK8@cluster0-ybspw.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true});
