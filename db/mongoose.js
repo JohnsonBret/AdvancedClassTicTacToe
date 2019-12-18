6a3608c9910ff6f071dae9ff05a1cae78c02edbd
Bret <bretaudio@gmail.com>
2019-12-17 18:12:11 -0800

    changed connection string

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://ucode_hermosa:<password>@cluster0-ybspw.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ucode_hermosa:SxaI2xLwgwY8KmK8@cluster0-ybspw.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true});
 mongoose.set('useFindAndModify', false);
