e99f3c5c75fc6805a4c997d54a3f8ae264091671
Bret Main <bretaudio@gmail.com>
2020-04-30 18:23:33 -0700

    your map route added

--- app.js
+++ app.js
@@ -147,6 +147,56 @@ app.get('/iamstealingyourinformation', async(req, res)=>{
     }
 });
 
+app.get('/your-house', async(req, res)=>{
+
+    try{
+        let ipAddress = req.header('x-forwarded-for') || req.connection.remoteAddress;
+        let ipURL = `https://json.geoiplookup.io/${ipAddress}`;
+        let result = await axios.get(ipURL);
+        res.status(200).send(`
+        <!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       /* Set the size of the div element that contains the map */
+      #map {
+        height: 400px;  /* The height is 400 pixels */
+        width: 100%;  /* The width is the width of the web page */
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <!--The div element for the map -->
+    <div id="map"></div>
+    <script>
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: ${result.data.latitude}, lng: ${result.data.longitude}};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+    </script>
+  </body>
+</html>`);
+    }
+    catch(e){
+        res.status(200).send(e);
+    }
+});
+
 app.get('/poker', async(req, res)=>{
 
     console.log(ip.address());
